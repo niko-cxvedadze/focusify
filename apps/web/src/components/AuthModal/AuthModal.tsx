c6db3a33e586db4854aa47e27e45a0bd8110028f
niko-cxvedadze <nikolozcxvedadze@gmail.com>
2025-04-20 19:22:55 +0400

    remove-unecessary-code

--- apps/web/src/components/AuthModal/AuthModal.tsx
+++ apps/web/src/components/AuthModal/AuthModal.tsx
@@ -1,18 +1,6 @@
 import { useAtom } from "jotai";
 import { authModalAtom } from "../../atoms/global.atoms";
-import {
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Button,
-  Checkbox,
-  Input,
-  Link,
-  Tabs,
-  Tab,
-} from "@heroui/react";
+import { Modal, ModalContent, ModalHeader, ModalBody, Button, Checkbox, Input, Link, Tabs, Tab } from "@heroui/react";
 
 export function AuthModal() {
   const [isOpen, setIsOpen] = useAtom(authModalAtom);
@@ -39,28 +27,4 @@ export function AuthModal() {
       </ModalContent>
     </Modal>
   );
-}
-
-function LoginForm() {
-    return (
-      <div>
-        <Input label="ელ-ფოსტა" placeholder="შეიყვანე თქვენი ელ-ფოსტა" variant="bordered" />
-        <Input label="პაროლი" placeholder="შეიყვანე თქვენი პაროლი" type="password" variant="bordered" />
-        <div className="flex justify-between px-1 py-1">
-          <Checkbox
-            classNames={{
-              label: "text-small",
-            }}
-          >
-            დამიმახსოვრე
-          </Checkbox>
-          <Link color="primary" href="#" size="sm">
-            დაგავიწყდა პაროლი?
-          </Link>
-        </div>
-        <Button color="primary" size="lg">
-          შესვლა
-        </Button>
-      </div>
-    );
 }
