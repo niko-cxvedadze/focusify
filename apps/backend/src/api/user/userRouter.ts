490fbc83f8849decdaeb1d568c3a86ccbf8e23f3
nikos1301 <nt@mind3.ch>
2025-07-10 23:37:33 +0400

    change

---
+++ apps/backend/src/api/user/userRouter.ts
@@ -0,0 +1,12 @@
+import express, { type Router } from 'express'
+
+import { GetUserSchema } from '@/api/user/userModel'
+import { validateRequest } from '@/common/utils/httpHandlers'
+
+import { userController } from './userController'
+
+export const userRouter: Router = express.Router()
+
+userRouter.get('/', userController.getUsers)
+
+userRouter.get('/:id', validateRequest(GetUserSchema), userController.getUser)
